510f8c273869123fdd9e6bc12d7d3914d09121eb
Felvenders <77592221+Felvenders@users.noreply.github.com>
2022-02-03 14:20:50 +0300

    Add files via upload

---
+++ WinAPI_client_cyber/WinAPI_client.cpp
@@ -0,0 +1,101 @@
+﻿// Client.cpp: определяет точку входа для консольного приложения.
+//
+
+#include <Windows.h>
+#include <tchar.h>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+    _wsetlocale(LC_ALL, L".866");
+
+    wcout << L"Введите имя сервера:";
+    TCHAR name_server[MAX_PATH] = { 0 };
+    wcin.getline(name_server, MAX_PATH);
+
+    // Формирование пути к серверу
+    wstring path = wstring(L"\\") + wstring(name_server) + wstring(L"\\pipe\\MyPipeName");
+
+        // Открытие сервера именованного канала
+        //HANDLE hFile = CreateFile(path.c_str(),                 // Путь к серверу
+        //    GENERIC_READ | GENERIC_WRITE,   // Режим открытия
+        //    NULL,                           // Монопольный доступ
+        //    NULL,                           // Защита по умочланию
+        //    OPEN_EXISTING,                  // Открытие существующего канала
+        //    NULL,                           // Запись-чтение синхронные
+        //    NULL);                          // Не требуется
+
+        HANDLE hFile = CreateNamedPipe(path.c_str(),                       // Имя сервера именованного канала
+            PIPE_ACCESS_DUPLEX,             // Чтение и запись
+            PIPE_TYPE_MESSAGE | PIPE_WAIT,  // Общение сообщениями в синхронном режиме
+            PIPE_UNLIMITED_INSTANCES,       // Количество клиентов, которые могут подключиться к создаваемому экземпляру именованного канала
+            NULL,                           // размер выходного буфера в байтах 
+            NULL,                           // размер входного буфера в байтах 
+            NULL,                           // время ожидания в миллисекундах 
+            NULL);
+
+    if (hFile == INVALID_HANDLE_VALUE)
+    {
+        wcout << L"Ошибка CreateFile = " << GetLastError() << endl;
+        wcin.get();
+        return 0;
+    }
+
+
+    wcout << L"Для завершение необходимо ввести команду exit" << endl;
+
+    // До тех пор, пока пользователь не введет exit
+    while (true)
+    {
+
+        wcout << L"Введите сообщение: ";
+        TCHAR buf[MAX_PATH] = { 0 };
+        wcin.getline(buf, MAX_PATH);
+
+        // Если пользователь ввел exit
+        if (!wcscmp(buf, L"exit")) // сравнение введеной строки с exit
+            break;
+
+        // Передача на сервер сообщения
+        DWORD write = NULL;
+        if (!WriteFile(hFile,                               // Дескриптор именованного канала
+            buf,                                // Указатель на буфер
+            wcslen(buf) * sizeof(TCHAR),    // Размер буфера в байтах
+            &write,                             // Сколько фактически передано байт
+            NULL))                          // Синхронная запись
+        {
+            wcout << L"Ошибка WriteFile = " << GetLastError() << endl;
+            wcin.get();
+            break;
+        }
+
+        // Прием данных от сервера
+        DWORD read = NULL;
+        memset(buf, 0, sizeof(buf));
+        if (!ReadFile(hFile,                // Дескриптор файла
+            buf,                // Буфер для данных
+            MAX_PATH - 1,       // Размер буфера - количество символов Unicode
+            &read,              // Фактический размер принятых данных
+            NULL))
+        {
+            wcout << L"Ошибка ReadFile = " << GetLastError() << endl;
+            wcin.get();
+            break;
+        }
+
+        // Вывод на консоль полученных данных
+        wcout << L"Сообщение сервера: ";
+        buf[read] = '\0';
+        wcout << buf << endl;
+
+    }
+
+    // Закрытие дескрипторов именованного канала
+    CloseHandle(hFile);
+
+    wcin.get();
+
+    return 0;
+}
---
+++ WinAPI_server_cyber/WinAPI_server.cpp
@@ -0,0 +1,113 @@
+﻿#include <Windows.h>
+#include <tchar.h>
+#include <iostream>
+using namespace std;
+
+// Имя сервера именованного канала
+LPCTSTR NAME_PIPE = _T("\\\\.\\pipe\\MyPipeName");
+
+DWORD WINAPI Thread(LPVOID);
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+    _wsetlocale(LC_ALL, L".866");
+    while (true)
+    {
+
+        // Создание нового экземпляра сервера именованного канала
+        HANDLE hPipe = CreateNamedPipe(NAME_PIPE,                       // Имя сервера именованного канала
+            PIPE_ACCESS_DUPLEX,             // Чтение и запись
+            PIPE_TYPE_MESSAGE | PIPE_WAIT,  // Общение сообщениями в синхронном режиме
+            PIPE_UNLIMITED_INSTANCES,       // Количество клиентов, которые могут подключиться к создаваемому экземпляру именованного канала
+            NULL,                           // размер выходного буфера в байтах 
+            NULL,                           // размер входного буфера в байтах 
+            NULL,                           // время ожидания в миллисекундах 
+            NULL);                           // Атрибуты безопасноти именованного канала
+        
+        // Проверка на создание канала
+        if (hPipe == INVALID_HANDLE_VALUE)
+        {
+            wcout << L"Ошибка CreateNamedPipe = " << GetLastError() << endl;
+            wcin.get();
+            break;
+        }
+
+        // Ожидание подключения клиента
+        if (ConnectNamedPipe(hPipe,     // Дескриптор именованного канала 
+            NULL))  // Синхронно
+        {
+            // Запуск отдельного потока, для обслуживания подключенного клиента
+            HANDLE hThread = CreateThread(NULL,     // Атрибут безопасности
+                NULL,       // Размер стека по умолчанию
+                Thread,     // Указатель на функцию потока
+                hPipe,      // Дескриптор именованного канала с подключенным клиентом
+                NULL,       // Запуск сразу
+                NULL);      // ID не требуется
+           
+                            // Проверка на создание потока
+            if (!hThread)
+            {
+                wcout << L"Ошибка CreateThread = " << GetLastError() << endl;
+                wcin.get();
+                break;
+            }
+
+            // Закрытие дескриптора потока
+            CloseHandle(hThread);
+
+        }
+        else
+            wcout << L"Error ConnectNamedPipe = " << GetLastError() << endl;
+    }
+
+    wcin.get();
+    return 0;
+}
+
+// Функция - поток для обслуживания клиентов
+DWORD WINAPI Thread(LPVOID param)
+{
+    // Дескриптор клиента
+    HANDLE hPipe = (HANDLE)param;
+
+    // Вечный цикл
+    while (true)
+    {
+        // Прием данных от клиента
+        DWORD read = NULL;
+        TCHAR buf[MAX_PATH] = { 0 };
+        if (!ReadFile(hPipe,            // Дескриптор именованного канала
+            buf,            // Указатель на буфер
+            MAX_PATH - 1,   // Размер буфера в символах Unicode
+            &read,          // Сколько байт полученного
+            NULL))
+        {
+            wcout << L"Ошибка ReadFile = " << GetLastError() << endl;
+            break;
+        }
+
+        wcout << L"Сообщение клиента: ";
+        buf[read / sizeof(TCHAR)] = '\0';
+        wcout << buf << endl;
+
+        // Передача ответа клиенту
+        wcout << L"Ваш ответ: ";
+        wcin.getline(buf, MAX_PATH);
+        DWORD write = NULL;
+        if (!WriteFile(hPipe,                               // Дескриптор именованного канала
+            buf,                                // Указатель на буфер передаваемых данных
+            wcslen(buf) * sizeof(TCHAR),    // Размер данных в байтах
+            &write,                             // Сколько байт передано
+            NULL))
+        {
+            wcout << L"Ошибка WriteFile = " << GetLastError() << endl;
+            break;
+        }
+    }
+
+    // Закрытие дескриптора именованного канала
+    CloseHandle(hPipe);
+
+    return 0;
+
+}
